6df257cd56a7b9f0ce44bd2bdc6c9756b2b548bc
Quentin Leclerc <quentin.leclerc@pasteur.fr>
2023-02-01 15:52:57 +0100

    qleclerc edit

--- awesome_script.R
+++ awesome_script.R
@@ -1,8 +1,23 @@
 
-# Awesome script to do clever data science!
+# Source this awesome script to do clever data science!
 
-data = rnorm(1000)
+user_name = readline("Hello there! Please type your username and press Enter: ")
 
-plot(data, type = "p", pch = 19, col = "red")
+fav_num = sample(10:100, 1)
+cat("Okay", user_name, "I'm going to guess your favourite number is...", fav_num, "!")
+cat("\n\nSo, I'll make a beautiful graph of a Poisson distribution with lambda =", fav_num)
 
-hist(data)
+data = rpois(1000, fav_num)
+hist(data, main = paste0("A beautiful graph for ", user_name))
+
+cat("\n\nLet me save this beautiful graph and the data behind it...")
+
+filename = paste0("data_",user_name,"_",format(Sys.time(), "%Hh%M"),".rds")
+saveRDS(data, here::here("data", filename))
+
+graphname = paste0("graph_",user_name,"_",format(Sys.time(), "%Hh%M"),".png")
+png(here::here("figures", graphname))
+hist(data, main = paste0("A beautiful graph for ", user_name))
+dev.off()
+
+cat("\n\nDone! Don't forget to commit the changes ;)")
